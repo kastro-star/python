ceaae059b24d04889c1e31d5bc589779e5e35a53
Kastro .R <kastrouchiha@gmail.com>
2025-01-30 06:19:04 -1200

    Add files via upload

---
+++ day2/logical operators.py
@@ -0,0 +1,25 @@
+#logical operators
+#or operator if one value is true it's true
+#AND operator two values are true it's true
+a = 5
+b = 10
+statement1 = (a==10)
+statement2 = (b==10)
+
+print(statement1 or statement2 )
+
+print(statement1 and statement2)
+'''AND OPERATOR
+T*T=TRUE
+T*F=FALSE
+F*T=FALSE
+F*F=FALSE
+
+
+OR OPERATORS
+
+T*T=TRUE
+T*F=TRUE
+F*T=TRUE
+F*F=FALSE
+'''
---
+++ day2/membership operators.py
@@ -0,0 +1,12 @@
+#Membership operators
+list = [1,2,3,4,5]
+name ="kastro"
+age = '70'
+print('5' in list) # to check the num in the list
+
+print('k' not in name)
+
+print('7' in age)
+
+print('7' in str(age))  # Convert age to a string
+
