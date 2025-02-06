ac586850d333c85ffd99ae55ea974b9e27afa93f
Kastro .R <kastrouchiha@gmail.com>
2025-02-05 15:05:19 -1200

    Update arithmetic operators.py

--- day2/arithmetic operators.py
+++ day2/arithmetic operators.py
@@ -2,9 +2,14 @@
 
 #a=9
 #b=3
-
+quotient  //
+remainder %
+divide /
+add +
+sub -
+square(2)2   **  
 #print(a/b)
-#print(10+5*3) #ans 25 divi -mul- add -sub order bodmas rule
+#print(10+5*3) #ans 25    orders the python solve them divi -mul- add -sub order bodmas rule
 #print(20-10/5*2)
 #print(10%5)    #%--module to find the remainder
 #print(a**b)  #to find the square value
