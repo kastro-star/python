827a5cf633ed0478c1be123be678156a00a975a4
Kastro .R <kastrouchiha@gmail.com>
2025-02-09 00:32:16 +0530

    Add files via upload

---
+++ day4/positive or negative.py
@@ -0,0 +1,20 @@
+#num = int(input("enter a number:"))
+#if num>0:
+    #print('positive number ')
+#elif num==0:
+    #print('zero')
+
+#else:
+        #print('negative number')
+
+
+
+x=5
+y=10
+if x>y:
+    largest = x
+else:
+    largest =y
+print("the largest number is",largest)    
+
+    
