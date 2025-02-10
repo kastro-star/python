6b70db8d1ac8b56ad323ee21fe47f4e85f3ef58c
Kastro .R <kastrouchiha@gmail.com>
2025-02-10 23:46:06 +0530

    Add files via upload

---
+++ day6/List.py
@@ -0,0 +1,42 @@
+# Lists in python
+
+#height_list=[176,186,199]
+#print(height_list)  #list is an comma separated collection of value enclosed withan square brackets
+
+
+'''
+height_list=[176,186,199 ,hii]  #list are used to store multiple kind of datatype
+print(height_list)                  #list values are accesed with their index values
+
+height_list=[176,186,199,
+             'hii']
+print(height_list[0])
+'''
+
+
+lst1=[1,2,3]
+lst2=[4,5,6]    #len keyword is used find the length of the list
+lst3=lst1+lst2
+
+print(len(lst3))
+
+
+
+lst1=[1,2,3]
+lst2=[4,5,6] 
+lst3=lst1+lst2
+#length of the list
+print(len(lst3))
+
+#inserting an element into the list append is store the value in the last of the list
+lst1.append(4)
+print(lst1)
+
+
+#extend keyword  is used to add the two list like add
+lst1.extend(lst2)
+print(lst1)
+
+#
+
+
